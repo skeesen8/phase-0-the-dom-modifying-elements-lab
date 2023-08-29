ed26807a2cd11a4ebaf1343ed19cf69540faa218
Sackett <skeesen8@gmail.com>
2023-08-29 14:58:30 -0500

    finished

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Writedocument.body.append(element); your code here!
+const main = document.getElementById("main");
+main.remove()
+// const secondChild = ul.querySelector("li:nth-child(2)");
+// ul.removeChild(secondChild);
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory')
+newHeader.textContent = "Sackett is the champion"
+console.log (newHeader)
+// const newHeader = document.getElementById("main");
+// main.innerHTML = 'victory'
+document.body.appendChild(newHeader)
